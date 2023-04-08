4dd6826e6b7ac092a68d8ac1d157fc39c7d9ec82
Ivashinko <ivashinkoalex@gmail.com>
2023-04-08 18:57:04 +0300

    Add file

---
+++ Ivashinko_Aleksey_home_work_L1.py
@@ -0,0 +1,51 @@
+# # Home work 1.0.
+# print('Hello world!')  # Вывод надписи Hello world!.
+# print("Hello, Python!")  # Вывод надписи Hello, Python!.
+
+# # Home work 1.1.
+# print('Programming', 'Essentionals', 'in', sep='***', end='...')  # Вывод первой части надписи с аргументами sep и end.
+# print('Python')  # Вывод второй части надписи без модификации функции print().
+
+# # Home work 1.2.
+# print("    ", "   * ", "  *   ", " *     ", "***   **", "  *   ",
+#             "  *   *", sep="*\n", end="\n  *****\n")  # Вывод стрелки с аргументами sep и end.
+
+# # Home work 1.3.
+# print('"I\'m"\n""learning""\n"""Python"""')  # Вывод надписи с использованием одной строки кода.
+
+# # Home work 1.4.
+# john_apples = 5
+# mary_apples = 44
+# adam_apples = 67
+
+# total_apples = john_apples + mary_apples + adam_apples
+
+# print(f'john: {john_apples}, mary: {mary_apples}, adam: {adam_apples}', sep=',')
+# print('Суммарное количество яблок:', total_apples, end='.\n')
+
+# # Home work 1.5.
+# kilometers = 12.25  # Исходные данные о км.
+# miles = 7.38  # Исходные данные о милях.
+# kilometer_in_mile = 1.61
+
+# result_miles = kilometers / kilometer_in_mile  # Мили в км.
+# result_kilometers = miles * kilometer_in_mile  # Км в милях.
+
+# print(miles, 'miles is', round(result_kilometers, 2), 'kilometers')
+# print(kilometers, 'kilometers is', round(result_miles, 2), 'miles')
+
+# # Home work 1.6.
+# # This program computes the number of seconds in a given number of hours.
+# hours = 2
+# seconds_in_hours = 3600
+
+# print("Hours: ", hours)  # Printing the number of hours.
+# print("Seconds in Hours: ", hours * seconds_in_hours)  # printing the number of seconds in a given number of hours.
+
+# # This is the end of the program that computes the number of seconds in 3 hour.
+# hours = 3
+
+# print("Hours: ", hours)
+# print("Seconds in Hours: ", hours * seconds_in_hours)
+
+# print("Goodbye")
